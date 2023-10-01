45eb3bbbadd2e665184d3a935f8488932fe77657
Plamen pandev <looopooo45@gmail.com>
2023-10-01 10:49:59 +0300

    Add project files.

---
+++ XMLParsing/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using System.Xml.XPath;
+using System.Xml;
+
+
+namespace HtmlParser
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            XmlDocument doc = new XmlDocument();
+            doc.Load(@"C:\Users\paco\Desktop\Project\XMLParsing\XMLParsing\sitemapbrowse.ashx");
+
+            XmlNodeList loc = doc.GetElementsByTagName("loc");
+            XmlNodeList mod = doc.GetElementsByTagName("lastmod");
+            XmlNodeList freq = doc.GetElementsByTagName("changefreq");
+            XmlNodeList prior = doc.GetElementsByTagName("priority");
+
+            string number = null;
+            int counter = 0;
+            int modNumber = 0;
+            List<List<string>> FileContent = new List<List<string>>();
+            for (int i = 0; i < doc.DocumentElement.ChildNodes.Count; i++)
+            {
+                number += mod[i].InnerText[14];
+                number+= mod[i].InnerText[15];
+                modNumber = int.Parse(number);
+                /*for (int j = 0; j < mod[i].InnerText.Length; j++)
+                {
+
+                     switch (mod[i].InnerText[j])
+                    {
+                        case ':':
+                            counter += 1;
+                            break;
+                        default:
+                            if (counter == 1)
+                            {
+                                number += mod[i].InnerText[j];
+                            }
+                            else if (counter > 1)
+                            {
+                                modNumber = int.Parse(number);
+                            }
+                            break;
+                    }
+                }*/
+                number = null;
+                counter = 0;
+                if (modNumber > 30)
+                {
+                    FileContent.Add(new List<string> { loc[i].InnerText, mod[i].InnerText, freq[i].InnerText, prior[i].InnerText });
+
+                }
+            }
+            /*XmlDocument doc2 = new XmlDocument();
+            doc2.Load(@"C:\Users\paco\Desktop\Project\XMLParsing\XMLParsing\xd.ashx");
+            XElement root = new XElement("url");
+            root.Add(new XAttribute("loc", loc[i].InnerText));
+            root.Add(new XAttribute("lastmod", mod[i].InnerText));
+            root.Add(new XAttribute("changefreq", freq[i].InnerText));
+            root.Add(new XAttribute("priority", prior[i].InnerText));
+            doc2.Save(@"C:\Users\ppandev\Desktop\XMLParser-master\xd.ashx");*/
+            foreach (List<string> line in FileContent)
+            {
+                foreach (string token in line)
+                {
+                    Console.Write($"{token}|");
+                }
+                Console.WriteLine();
+            }
+
+            
+        }
+    }
+}
---
+++ XMLParsing/xd.ashx
@@ -0,0 +1,7 @@
+﻿<urlset xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/siteindex.xsd">
+<url>
+<loc>https://usdirectory.com/Florida/panama-city-beach/Flooring-Contractors</loc>
+<lastmod>2023-09-29T07:44:32.650+00:00</lastmod>
+<changefreq>Yearly</changefreq>
+<priority>0.5</priority>
+</url>
