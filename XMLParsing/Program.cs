a9c0dd1ff7cc3258c814a616e8fba0bf286f9b61
Plamen pandev <looopooo45@gmail.com>
2023-10-02 19:55:05 +0300

    the code is more optimized

--- XMLParsing/Program.cs
+++ XMLParsing/Program.cs
@@ -5,10 +5,8 @@ using System.Net;
 using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
-using System.Xml.Linq;
-using System.Xml.XPath;
-using System.Xml;
 using System.IO;
+using HtmlAgilityPack;
 
 
 namespace HtmlParser
@@ -17,39 +15,26 @@ namespace HtmlParser
     {
         static void Main(string[] args)
         {
+            string xml = @"C:\Users\paco\Desktop\Project\XMLParsing\XMLParsing\sitemapbrowse.ashx";
 
-            XmlDocument doc = new XmlDocument();
-            doc.Load(@"C:\Users\paco\Desktop\Project\XMLParsing\XMLParsing\sitemapbrowse.ashx");
-            XDocument doc2 = XDocument.Load(@"C:\Users\paco\Desktop\Project\XMLParsing\XMLParsing\xd.ashx");
+            HtmlWeb web = new HtmlWeb();
 
-            XmlNodeList loc = doc.GetElementsByTagName("loc");
-            XmlNodeList mod = doc.GetElementsByTagName("lastmod");
-            XmlNodeList freq = doc.GetElementsByTagName("changefreq");
-            XmlNodeList prior = doc.GetElementsByTagName("priority");
+            var doc = web.Load(xml);
+            var link = doc.DocumentNode.SelectNodes(@"/urlset/url");
 
-            string number = null;
-            int modNumber = 0;
-            List<List<string>> FileContent = new List<List<string>>();
-            for (int i = 0; i < doc.DocumentElement.ChildNodes.Count; i++)
+            var result = new HtmlDocument();
+            if (link != null)
             {
-                number += mod[i].InnerText[14];
-                number+= mod[i].InnerText[15];
-                modNumber = int.Parse(number);
-                number = null;
-                if (modNumber > 30)
+                foreach (var node in link)
                 {
-                    
-
-                    XElement root = new XElement("url");
-
-                    root.Add(new XElement("loc",loc[i].InnerText));
-                    root.Add(new XElement("lastmod",mod[i].InnerText));
-                    root.Add(new XElement("changefreq",freq[i].InnerText));
-                    root.Add(new XElement("priority",prior[i].InnerText));
-                    doc2.Element("urlset").Add(root);
-                    doc2.Save(@"C:\Users\paco\Desktop\Project\XMLParsing\XMLParsing\xd.ashx");
-
+                    var lastmodNode = node.SelectSingleNode("./lastmod");
+                    DateTime dt = DateTime.Parse(lastmodNode.InnerText);
+                    if (dt > DateTime.Now.AddDays(-30))
+                    {
+                        result.DocumentNode.AppendChild(node);
+                    }
                 }
+                result.Save("result.xml");
             }
         }
     }
